6724199769750b4c683c1e7cd7740e9eb5ab71a3
Noah Sandman <nsandman09@gmail.com>
2015-04-18 16:44:08 -0700

    Better parsing, added libcurl headers

--- gpm.c
+++ gpm.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <curl/curl.h>
 
 int main(int argc, const char *argv[]) {
-	const char *mainCmd = argv[1];
-	if (strcmp(mainCmd, "install") == 0) {
+	char *gpmdir = strcat(getpwuid(getuid())->pw_dir, "/.gpm");
+	if (strcmp(argv[1], "install") == 0) {
 		for (int a = 2; a < argc; a++) {
-			printf("\033[0;32mInstalling package %s...\n\033[0m", argv[a]);
-
+			char *pkg = argv[a];
+			printf("\033[0;32mInstalling package %s...\n\033[0m", pkg);
+			char *file = "%s/pkg/%s.gpm", gpmdir, argv[a];
+			char *pkgfile = fopen(file, "r");
 		}
 	}
 	else {
-		printf("\033[0;31mUnknown command: %s %s \033[0m\n", argv[0], mainCmd);
+		printf("\033[0;31mUnknown command: %s %s \033[0m\n", argv[0], argv[1]);
 	}
 	return 0;
 }
