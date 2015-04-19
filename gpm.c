d458aa8cac1351be0d1e26f4a043ec2f63dc963b
Noah Sandman <nsandman09@gmail.com>
2015-04-18 18:44:14 -0700

    Old Python source code

--- gpm.c
+++ gpm.c
@@ -20,31 +20,35 @@
    -ng. This probably will not work with other versions because of st
    -ring formatting done weirdly and VLAs with malloc. */
 
+char *pkgURL;
+
 int main(int argc, char *argv[]) {
 	CURL *curl;
 	CURLcode res;
 	
 	char *gpmdir = strcat(getpwuid(getuid())->pw_dir, "/.gpm"); /* Get GPM file dir (~/.gpm) */
-	if (strcmp(argv[1], "install") == 0) { /* See if first argument is "install" */
+	if (strcmp(argv[1], "install") == 0 || strcmp(argv[1], "i") == 0) {
 		for (int a = 2; a < argc; a++) {
+			pkgURL = malloc(68 + strlen(argv[a]));
 			curl = curl_easy_init();
 			printf("\033[0;32mInstalling package %s...\n\033[0m", argv[a]);
-			char *pkgURL = malloc(68 + strlen(argv[a]));
 			strcat(pkgURL, "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/");
 			strcat(pkgURL, argv[a]);
 			strcat(pkgURL, ".gpm");
 			if (curl) {
-				curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0");
 				curl_easy_setopt(curl, CURLOPT_URL, pkgURL);
 				res = curl_easy_perform(curl);
+				curl_easy_cleanup(curl);
+				if (res == NULL) {
+					printf(stderr, "\033[0;31mPackage \"%s\" not found!\033[0m", argv[a]);
+				}
 			}
-			free(pkgURL);
+			pkgURL = "";
 		}
 	}
 	else {
-		// Otherwise, print "unknown command" error
+		/* Otherwise, print "unknown command" error */
 		printf(stderr, "\033[0;31mUnknown command: %s %s \033[0m\n", argv[0], argv[1]);
 	}
-	// Exit with error code 0
 	return 0;
 }
