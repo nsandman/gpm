fc53faffd0973a614b5339d0e5615c653dea0303
Noah Sandman <nsandman09@gmail.com>
2015-04-19 14:57:40 -0700

    Added URL fetching with libcurl again

--- gpm.cpp
+++ gpm.cpp
@@ -11,19 +11,41 @@
  ––––––––––––––––––––––
 */
 #include <iostream>
-using namespace std;
+#include <curl/curl.h>
+
+std::string returnedText;
+std::string currentArg;
+std::string pkgURL;
+
+/* By "heikaman" from http://ubuntuforums.org/archive/index.php/t-1435926.html */
+size_t curl_write(void *ptr, size_t size, size_t nmemb, void *stream) {
+	returnedText.append((char*)ptr);
+	return size*nmemb;
+}
 
 int main(int argc, const char *argv[]) {
-	string arg = argv[0];
-	string initialArg = argv[1];
+	CURL *curl = curl_easy_init();
+	std::string arg = argv[0];
+	std::string initialArg = argv[1];
 	if (initialArg == "install" || initialArg == "i") {
 		for (int a = 2; a < argc; a++) {
-			string currentArg = argv[a];
-			cout << "\033[00;32mInstalling package \"" << currentArg << "\"...\033[0m\n";
-				string pkgURL = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + currentArg + ".gpm";
-				cout << pkgURL;
+			currentArg = argv[a];
+			std::cout << "\033[00;32mInstalling package \"" << currentArg << "\"...\033[0m\n";
+			pkgURL = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + currentArg + ".gpm";
+			curl_easy_setopt(curl, CURLOPT_URL, pkgURL.c_str()); 
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write);
+			curl_easy_perform(curl);
+			if (returnedText == "Not Found") {
+				std::cout << "\033[00;31mThe package \"" << currentArg << "\" was not found.\033[0m\n";
+			} else {
+				std::cout << returnedText;
+			}
+			returnedText = "";
 		}
 	} else {
-		cout << "\033[00;31mCommand \033[1;31m" + arg + " " + initialArg + " \033[0m\033[00;31mnot found.\033[0m\n";
+		std::cout << "\033[00;31mCommand \033[1;31m" + arg + " " + initialArg + " \033[0m\033[00;31mnot found.\033[0m\n";
 	}
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
+	return 0;
 }
