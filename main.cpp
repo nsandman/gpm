b9f6380e5aeeefa36db56164be47cc3c0f468153
Noah Sandman <nsandman09@gmail.com>
2015-04-25 16:02:06 -0700

    Successfully installs to "tmp" directory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /* main.cpp - Noah Sandman
-   04/24/2014 */
+   04/24/2015 */
 
 #include <cstring>
 #include <fstream>
@@ -10,12 +10,14 @@
 #include "rapidjson/document.h"
 using namespace std;
 
-int main(int argc, const char *argv[]) {
+int main(int argc, char *argv[]) {
+	string GPMDIR = strcat(getenv("HOME"), "/.gpm");
 	CURL *curl = curl_easy_init();
-	const char *argOne = argv[1];
+	char *argOne = argv[1];
 	if (strcmp(argOne, "install") == 0 || strcmp(argOne, "i") == 0) {
 		for (int a = 2; a < argc; a++) {
 			string ca = argv[a];
+			string cFile = GPMDIR + "/tmp/" + ca;
 			string ghUrl = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + ca + ".gpm";
 			cc();
 			curl_easy_setopt(curl, CURLOPT_URL, ghUrl.c_str());
@@ -24,10 +26,21 @@ int main(int argc, const char *argv[]) {
 			rapidjson::Document getUrl;
 			getUrl.Parse(curlResult.c_str());
 			rapidjson::Value &url = getUrl["url"];
+			rapidjson::Value &cmd = getUrl["commands"];
 			cc();
 			curl_easy_setopt(curl, CURLOPT_URL, url.GetString());
 			curl_easy_perform(curl);
-			cout << curlResult << endl;
+			ofstream downloadFile;
+  			downloadFile.open(cFile);
+  			downloadFile << curlResult;
+  			downloadFile.close();
+  			for (int b = 0; b < cmd.Size(); b++) {
+				string currentCommand(cmd[b].GetString());
+				currentCommand = replaceAll(currentCommand, string("{FILENAME}"), cFile);
+				currentCommand = replaceAll(currentCommand, string("{GPMDIR}"), GPMDIR);
+				cout << currentCommand << endl;
+				system(currentCommand.c_str());
+			}
 		}
 		curl_easy_cleanup(curl);
 		curl_global_cleanup();
