41f4017b69ac99c50971439fcd4f163133409acc
Noah Sandman <nsandman09@gmail.com>
2015-04-25 17:49:06 -0700

    Fixed segfault on no params, added colors

--- main.cpp
+++ main.cpp
@@ -15,74 +15,79 @@ int main(int argc, char *argv[]) {
 	CURL *curl = curl_easy_init();
 	/* Use callback to save cURL output to variable */
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, nFunctions::curlToVar);
-	char *argOne = argv[1];
-	const char *iDir = string(GPMDIR + "/installed/_installed.gpi").c_str();
-	if (strcmp(argOne, "install") == 0 || strcmp(argOne, "i") == 0) {
-		for (int a = 2; a < argc; a++) {
-			bool shouldContinue = true;
-			string ca = argv[a];
+	if (argc > 1) {
+		char *argOne = argv[1];
+		const char *iDir = string(GPMDIR + "/installed/_installed.gpi").c_str();
+		if (strcmp(argOne, "install") == 0 || strcmp(argOne, "i") == 0) {
+			for (int a = 2; a < argc; a++) {
+				bool shouldContinue = true;
+				string ca = argv[a];
+				ifstream installed;
+				installed.open(iDir);
+				string line;
+				for (int i = 0; getline(installed, line); i++) {
+					if (strcmp(line.c_str(), ca.c_str()) == 0) {
+						shouldContinue = false;
+						break;
+					}
+				}
+				installed.close();
+				if (shouldContinue) {
+					cout << "\033[0;32mPackage \"" << ca << "\" found, installing...\033[0m\n";
+					string cFile = GPMDIR + "/tmp/" + ca;
+					string ghUrl = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + ca + ".gpm";
+					/* Clear the cURL response variable */
+					nFunctions::cc();
+					curl_easy_setopt(curl, CURLOPT_URL, ghUrl.c_str());
+					curl_easy_perform(curl);
+					rapidjson::Document getUrl;
+					getUrl.Parse(curlResult.c_str());
+					rapidjson::Value &url = getUrl["url"];
+					rapidjson::Value &cmd = getUrl["commands"];
+					nFunctions::cc();
+					curl_easy_setopt(curl, CURLOPT_URL, url.GetString());
+					curl_easy_perform(curl);
+					ofstream downloadFile;
+		  			downloadFile.open(cFile.c_str());
+		  			downloadFile << curlResult;
+		  			downloadFile.close();
+		  			for (int b = 0; b < cmd.Size(); b++) {
+						string currentCommand(cmd[b].GetString());
+						/* Replace the variables */
+						currentCommand = nFunctions::replaceAll(currentCommand, string("{FILENAME}"), cFile);
+						currentCommand = nFunctions::replaceAll(currentCommand, string("{GPMDIR}"), GPMDIR);
+						system(currentCommand.c_str()); /* Execute the edited command */
+					}
+					/* Move from "tmp" to "installed" using nFunctions::replaceAll() in rename() */
+					rename(cFile.c_str(), nFunctions::replaceAll(cFile, string("/tmp/"), string("/installed/")).c_str());
+					/* Append to installed files list */
+					ofstream installed;
+					installed.open(iDir, ios::app);
+					installed << ca << endl;
+					installed.close();
+				} else {
+					cout << "\033[0;31mPackage \"" << ca << "\" already installed!\033[0m\n";
+				}
+			}
+			/* Clean up! */
+			curl_easy_cleanup(curl);
+			curl_global_cleanup();
+		} 
+		else if (strcmp(argOne, "list") == 0 || strcmp(argOne, "l") == 0) {
 			ifstream installed;
 			installed.open(iDir);
-			string line;
-			for (int i = 0; getline(installed, line); i++) {
-				if (strcmp(line.c_str(), ca.c_str()) == 0) {
-					shouldContinue = false;
-					break;
-				}
+			string iOutput;
+			string cLine;
+			for (int i = 0; getline(installed, cLine); i++) {
+				iOutput.append(cLine + "\n");
 			}
-			installed.close();
-			if (shouldContinue) {
-				string cFile = GPMDIR + "/tmp/" + ca;
-				string ghUrl = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + ca + ".gpm";
-				/* Clear the cURL response variable */
-				nFunctions::cc();
-				curl_easy_setopt(curl, CURLOPT_URL, ghUrl.c_str());
-				curl_easy_perform(curl);
-				rapidjson::Document getUrl;
-				getUrl.Parse(curlResult.c_str());
-				rapidjson::Value &url = getUrl["url"];
-				rapidjson::Value &cmd = getUrl["commands"];
-				nFunctions::cc();
-				curl_easy_setopt(curl, CURLOPT_URL, url.GetString());
-				curl_easy_perform(curl);
-				ofstream downloadFile;
-	  			downloadFile.open(cFile.c_str());
-	  			downloadFile << curlResult;
-	  			downloadFile.close();
-	  			for (int b = 0; b < cmd.Size(); b++) {
-					string currentCommand(cmd[b].GetString());
-					/* Replace the variables */
-					currentCommand = nFunctions::replaceAll(currentCommand, string("{FILENAME}"), cFile);
-					currentCommand = nFunctions::replaceAll(currentCommand, string("{GPMDIR}"), GPMDIR);
-					system(currentCommand.c_str()); /* Execute the edited command */
-				}
-				/* Move from "tmp" to "installed" using nFunctions::replaceAll() in rename() */
-				rename(cFile.c_str(), nFunctions::replaceAll(cFile, string("/tmp/"), string("/installed/")).c_str());
-				/* Append to installed files list */
-				ofstream installed;
-				installed.open(iDir, ios::app);
-				installed << ca << endl;
-				installed.close();
-			} else {
-				cout << "Package \"" << ca << "\" already installed!\n";
-			}
+			cout << "\033[1;30mINSTALLED PACKAGES\n==================\033[0m\n" << iOutput << endl;
 		}
-		/* Clean up! */
-		curl_easy_cleanup(curl);
-		curl_global_cleanup();
-	} 
-	else if (strcmp(argOne, "list") == 0 || strcmp(argOne, "l") == 0) {
-		ifstream installed;
-		installed.open(iDir);
-		string iOutput;
-		string cLine;
-		for (int i = 0; getline(installed, cLine); i++) {
-			iOutput.append(cLine + "\n");
+		else {
+			cout << "\033[0;31mUnknown param " << argOne << "!\033[0m\n";
 		}
-		cout << "INSTALLED PACKAGES\n==================\n" << iOutput << endl;
-	}
-	else {
-		cout << "Unknown param " << argOne << "!\n";
+	} else {
+		cout << "\033[0;31mNo params given!\033[0m\n";
 	}
 	return 0;
 }
