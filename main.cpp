3c9adf67c8ca1baaccfabec767f8d1619a8f2c81
Noah Sandman <nsandman09@gmail.com>
2015-04-25 17:34:45 -0700

    Added checking and "gpm list"

--- main.cpp
+++ main.cpp
@@ -13,38 +13,75 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	string GPMDIR = strcat(getenv("HOME"), "/.gpm");
 	CURL *curl = curl_easy_init();
+	/* Use callback to save cURL output to variable */
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, nFunctions::curlToVar);
 	char *argOne = argv[1];
+	const char *iDir = string(GPMDIR + "/installed/_installed.gpi").c_str();
 	if (strcmp(argOne, "install") == 0 || strcmp(argOne, "i") == 0) {
 		for (int a = 2; a < argc; a++) {
+			bool shouldContinue = true;
 			string ca = argv[a];
-			string cFile = GPMDIR + "/tmp/" + ca;
-			string ghUrl = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + ca + ".gpm";
-			cc();
-			curl_easy_setopt(curl, CURLOPT_URL, ghUrl.c_str());
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlToVar);
-			curl_easy_perform(curl);
-			rapidjson::Document getUrl;
-			getUrl.Parse(curlResult.c_str());
-			rapidjson::Value &url = getUrl["url"];
-			rapidjson::Value &cmd = getUrl["commands"];
-			cc();
-			curl_easy_setopt(curl, CURLOPT_URL, url.GetString());
-			curl_easy_perform(curl);
-			ofstream downloadFile;
-  			downloadFile.open(cFile);
-  			downloadFile << curlResult;
-  			downloadFile.close();
-  			for (int b = 0; b < cmd.Size(); b++) {
-				string currentCommand(cmd[b].GetString());
-				currentCommand = replaceAll(currentCommand, string("{FILENAME}"), cFile);
-				currentCommand = replaceAll(currentCommand, string("{GPMDIR}"), GPMDIR);
-				cout << currentCommand << endl;
-				system(currentCommand.c_str());
+			ifstream installed;
+			installed.open(iDir);
+			string line;
+			for (int i = 0; getline(installed, line); i++) {
+				if (strcmp(line.c_str(), ca.c_str()) == 0) {
+					shouldContinue = false;
+					break;
+				}
+			}
+			installed.close();
+			if (shouldContinue) {
+				string cFile = GPMDIR + "/tmp/" + ca;
+				string ghUrl = "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/" + ca + ".gpm";
+				/* Clear the cURL response variable */
+				nFunctions::cc();
+				curl_easy_setopt(curl, CURLOPT_URL, ghUrl.c_str());
+				curl_easy_perform(curl);
+				rapidjson::Document getUrl;
+				getUrl.Parse(curlResult.c_str());
+				rapidjson::Value &url = getUrl["url"];
+				rapidjson::Value &cmd = getUrl["commands"];
+				nFunctions::cc();
+				curl_easy_setopt(curl, CURLOPT_URL, url.GetString());
+				curl_easy_perform(curl);
+				ofstream downloadFile;
+	  			downloadFile.open(cFile.c_str());
+	  			downloadFile << curlResult;
+	  			downloadFile.close();
+	  			for (int b = 0; b < cmd.Size(); b++) {
+					string currentCommand(cmd[b].GetString());
+					/* Replace the variables */
+					currentCommand = nFunctions::replaceAll(currentCommand, string("{FILENAME}"), cFile);
+					currentCommand = nFunctions::replaceAll(currentCommand, string("{GPMDIR}"), GPMDIR);
+					system(currentCommand.c_str()); /* Execute the edited command */
+				}
+				/* Move from "tmp" to "installed" using nFunctions::replaceAll() in rename() */
+				rename(cFile.c_str(), nFunctions::replaceAll(cFile, string("/tmp/"), string("/installed/")).c_str());
+				/* Append to installed files list */
+				ofstream installed;
+				installed.open(iDir, ios::app);
+				installed << ca << endl;
+				installed.close();
+			} else {
+				cout << "Package \"" << ca << "\" already installed!\n";
 			}
 		}
+		/* Clean up! */
 		curl_easy_cleanup(curl);
 		curl_global_cleanup();
-	} else {
+	} 
+	else if (strcmp(argOne, "list") == 0 || strcmp(argOne, "l") == 0) {
+		ifstream installed;
+		installed.open(iDir);
+		string iOutput;
+		string cLine;
+		for (int i = 0; getline(installed, cLine); i++) {
+			iOutput.append(cLine + "\n");
+		}
+		cout << "INSTALLED PACKAGES\n==================\n" << iOutput << endl;
+	}
+	else {
 		cout << "Unknown param " << argOne << "!\n";
 	}
 	return 0;
--- main.h
+++ main.h
@@ -3,21 +3,23 @@
 
 static std::string curlResult;
 
-std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
-    size_t start_pos = 0;
-    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-        str.replace(start_pos, from.length(), to);
-        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
-    }
-    return str;
-}
+namespace nFunctions {
+	std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
+	    size_t start_pos = 0;
+	    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+	        str.replace(start_pos, from.length(), to);
+	        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+	    }
+	    return str;
+	}
 
-static size_t curlToVar(void *ptr, size_t size, size_t nmemb, ...) {
-	curlResult.append((char*)ptr, size*nmemb);
-	return size * nmemb;
-}
+	static size_t curlToVar(void *ptr, size_t size, size_t nmemb, ...) {
+		curlResult.append((char*)ptr, size*nmemb);
+		return size * nmemb;
+	}
 
-/* This is just a quick function to clear cURL results */
-void cc(void) {
-	curlResult.clear();
+	/* This is just a quick function to clear cURL results */
+	void cc(void) {
+		curlResult.clear();
+	}
 }
