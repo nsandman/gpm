88281086a85596589eca440b6f0e8e5c9afe3e38
Noah Sandman <nsandman09@gmail.com>
2015-04-29 00:06:10 -0700

    Cleaned up code, colors, run commands

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 int main(int argc, char *argv[]) {
-	const char *gpmdir = strcat(getenv("HOME"), "/.gpm");
+	gpmdir = strcat(getenv("HOME"), "/.gpm");
 	dlUrl = malloc(1);
+	downloadedFileName = malloc(1);
 	curl = curl_easy_init();
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlToVar);
 	if (argc > 1) {
@@ -10,35 +11,51 @@ int main(int argc, char *argv[]) {
 			for (int a = 2; a < argc; a++) {
 				char *currentArg = argv[a];
 				dlUrl = realloc(dlUrl, 68 + strlen(currentArg));
-				strcat(dlUrl, "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/");
+				strcpy(dlUrl, "https://raw.githubusercontent.com/nsandman09/gpm-packages/master/");
 				strcat(dlUrl, currentArg);
 				strcat(dlUrl, ".gpm");
 				clearVar(CurlResult, 1);
 				curl_easy_setopt(curl, CURLOPT_URL, dlUrl);
 				curl_easy_perform(curl);
-				printf("%s\n", CurlResult);
-				jCurlParse = cJSON_Parse(CurlResult);
-				cmds = cJSON_GetObjectItem(jCurlParse,"commands");
-				char *urlFromJson = cJSON_Print(cJSON_GetObjectItem(jCurlParse,"url"));
-				clearVar(CurlResult, 1);
-				curl_easy_setopt(curl, CURLOPT_URL, urlFromJson);
-				curl_easy_perform(curl);
-				for (int b = 0; b < cJSON_GetArraySize(cmds); b++) {
-					printf("%s\n", cJSON_Print(cJSON_GetArrayItem(cmds, b)));
-					/* system(cJSON_Print(cJSON_GetArrayItem(cmds, b))); */
+				if (strcmp(CurlResult, "Not Found") != 0) {
+					printf("\033[0;32mPackage \"%s\" found, installing...\033[0m\n", currentArg);
+					jCurlParse = cJSON_Parse(CurlResult);
+					cmds = cJSON_GetObjectItem(jCurlParse,"commands");
+					char *urlFromJson = cJSON_Print(cJSON_GetObjectItem(jCurlParse,"url"));
+					clearVar(CurlResult, 1);
+					curl_easy_setopt(curl, CURLOPT_URL, urlFromJson);
+					curl_easy_perform(curl);
+					downloadedFileName = realloc(downloadedFileName, strlen(gpmdir) + strlen(currentArg) + 5);
+					strcpy(downloadedFileName, gpmdir);
+					strcat(downloadedFileName, "/tmp/");
+					downloadedFile = fopen(strcat(downloadedFileName, currentArg), "w");
+					fprintf(downloadedFile, "%s", CurlResult);
+					fclose(downloadedFile);
+					for (int b = 0; b < cJSON_GetArraySize(cmds); b++) {
+						char *tmpCmd = cJSON_Print(cJSON_GetArrayItem(cmds, b));
+						replaceAll(tmpCmd, "{FILENAME}", downloadedFileName);
+						replaceAll(tmpCmd, "{GPMDIR}", gpmdir);
+						system(tmpCmd);
+					}
+				} else {
+					printf("\033[0;31mPackage \"%s\" not found, skipping...\033[0m\n", currentArg);
 				}
 			}
 		} else {
-			printf("Unknown param \"%s\"!\n", (char*)argv[1]);
+			printf("\033[0;31mUnknown param \"%s\"!\033[0m\n", (char*)argv[1]);
+			exit(0);			/* Avoid "Done." being printed */
 		}
 	} else {
-		printf("No params entered!\n");
+		printf("\033[0;31mNo params entered!\033[0m\n");
+		exit(0);
 	}
 	/* Run function cleanups */
 	cleanupMain();
 	cJSON_Delete(jCurlParse);
 	curl_easy_cleanup(curl);
 	curl_global_cleanup();
-	free(dlUrl);			/* This is done manually, for some reason */
+	free(dlUrl);			/* This must be done manually, for some reason */
+	free(downloadedFileName);
+	printf("Done.\n");
 	return 0;
 }
--- main.h
+++ main.h
@@ -6,15 +6,26 @@
 #include <stdlib.h>
 #include <curl/curl.h>
 
+#ifdef __APPLE__
+#include <Python/Python.h>
+#else 
+#include <Python.h>
+#endif
+#define MAX_L 4096
+
 #include "json.h"
 
 char *CurlResult;		/* Blank string for cURL result */
 char *currentChunk;
 char *dlUrl;
+char *downloadedFileName;
 cJSON *jCurlParse;
 cJSON *cmds;
 CURL *curl;
+FILE *downloadedFile;
 int timesRun; 				/* How many times have we run the function? */
+
+char *gpmdir;
 
 size_t curlToVar(void *ptr, size_t size, size_t nmemb, ...) {
 	currentChunk = malloc(strlen((char*)ptr)); 					/* Save value of ptr to variable */
@@ -24,11 +35,24 @@ size_t curlToVar(void *ptr, size_t size, size_t nmemb, ...) {
 		strcpy(CurlResult, currentChunk);
 	} else {																	/* Else, use realloc() and strncat() */
 		CurlResult = realloc(CurlResult, strlen(CurlResult) + strlen(currentChunk));
-		strncat(CurlResult, currentChunk, size*nmemb); 
+		strncat(CurlResult, currentChunk, size * nmemb);		/* Only concatenate proper number to avoid overrun */
 	}
 	timesRun++;
 	return size * nmemb; 			/* Return bytes left to write (for cURL) */
 }
+
+void replaceAll(char * o_string, char * s_string, char * r_string) {
+      char buffer[MAX_L];
+      char * ch;
+
+      if(!(ch = strstr(o_string, s_string))) return;
+      strncpy(buffer, o_string, ch-o_string);
+      buffer[ch-o_string] = 0;
+      sprintf(buffer+(ch - o_string), "%s%s", r_string, ch + strlen(s_string));
+      o_string[0] = 0;
+      strcpy(o_string, buffer);
+      return replaceAll(o_string, s_string, r_string);
+ }
 
 void clearVar(void *varToClear, int reset) {
 	if (reset == 1) timesRun = 0;				/* If resetting for cUrl, set timesRun to 0 */	
